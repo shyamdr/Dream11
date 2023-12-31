512905872d6d85f252fb8f6fbff90292a12159b8
shyamdr <shyamdrangapure@gmail.com>
2023-12-31 23:32:37 +0530

    Updating latitude and longitude information in ground table in dwh

--- project/config/database/dwh/tables/ground.sql
+++ project/config/database/dwh/tables/ground.sql
@@ -4,12 +4,14 @@
 
 CREATE TABLE IF NOT EXISTS dwh.ground
 (
-    ground_id integer NOT NULL DEFAULT nextval('dwh.ground_ground_id_seq'::regclass),
+    ground_id serial,
     city text COLLATE pg_catalog."default",
     venue text COLLATE pg_catalog."default",
     country text COLLATE pg_catalog."default",
     region text COLLATE pg_catalog."default",
     active boolean DEFAULT true,
+	longitude double precision,
+    latitude double precision,
     CONSTRAINT ground_pk PRIMARY KEY (ground_id),
     CONSTRAINT ground_uk UNIQUE (venue)
 )
